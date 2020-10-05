67151899d49adbe9f9dc33288758bc6149b52c13
SCB8738 <ckdqja135@naver.com>
2020-10-05 16:09:16 +0900

    2020-10-05

---
+++ mdfile/2020-10-05/SQL query(CRUD) 풀소스/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"./app"
+
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	m := app.MakeHandler("./test.db")
+	defer m.Close()
+	n := negroni.Classic()
+	n.UseHandler(m)
+
+	log.Println("Started App")
+	err := http.ListenAndServe(":3000", n)
+	if err != nil {
+		panic(err)
+	}
+}
